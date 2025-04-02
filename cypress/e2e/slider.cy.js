58b27590e7cb991e917ebb829637d884299e2ec0
Alex555eu <aleksanderpasichofficial@gmail.com>
2025-04-02 21:20:28 +0200

    Added e2e test and 'hotjar' header script

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,62 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Navigates through slides using next and previous buttons', function () {
+    cy.visit('http://localhost:3000');
+  
+    cy.get('.swiper-slide-active .card-description h1')
+      .invoke('text')
+      .then((initialTitle) => {
+
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000); 
+
+        cy.get('.swiper-slide-active .card-description h1')
+          .invoke('text')
+          .then((newTitle) => {
+            expect(newTitle).to.not.eq(initialTitle);
+          });
+  
+        cy.get('.swiper-button-prev').click();
+        cy.wait(2000);
+  
+        cy.get('.swiper-slide-active .card-description h1')
+          .invoke('text')
+          .then((returnedTitle) => {
+            expect(returnedTitle).to.eq(initialTitle);
+          });
+      });
+  });
+
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Verifies each slide has title and description', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide').each(($slide) => {
+      cy.wrap($slide).within(() => {
+        cy.get('.card-description h1')
+          .and('not.be.empty');
+        cy.get('.card-description p')
+          .and('not.be.empty');
+      });
+    });
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks gallery responsiveness on different devices', function () {
+    const viewports = [[1280, 720], [768, 1024], [375, 667]];
+    viewports.forEach((size) => {
+      cy.viewport(size[0], size[1]);
+      cy.visit('http://localhost:3000');
+      cy.get('.wrapper').should('be.visible');
+      cy.get('.swiper-button-next').should('be.visible');
+      cy.get('.swiper-button-prev').should('be.visible');
+    });
+  });
+});
+
+
